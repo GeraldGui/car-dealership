328ab740387caffbd444acc9bb7290f5d21836e1
Gerald G <geraldguignard2@gmail.com>
2026-05-12 15:42:22 -0400

    - Made `Vehicle`, `Dealership`, `UserInterface`, and `DealershipFileManager` classes. - Made the `Vehicle` class start with the attributes, constructor and Getters/Setters. - Made the `Dealership` class have Getters/Setters, and all the methods for the class to be prepared to be used with others. - Instantiate the ArrayList<Vehicle> in `Dealership` also return the inventory with `getAllVehicles` and then you can use `addVehicle` as well.

---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,91 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Vehicle {
+    private int vin;
+    private  int year;
+    private int odometer;
+    private double price;
+    private String make;
+    private String model;
+    private String color;
+
+    private String vehicleType;
+
+    public Vehicle(int vin, int year, int odometer, double price, String make, String model, String color, String vehicleType) {
+        this.vin = vin;
+        this.year = year;
+        this.odometer = odometer;
+        this.price = price;
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.vehicleType = vehicleType;
+    }
+
+    public int getVin() {
+        return vin;
+    }
+
+    public void setVin(int vin) {
+        this.vin = vin;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+}
